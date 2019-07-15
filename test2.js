72e945f1ddadd73558824e8005eefb94069a657e
sebbeth <seb.brown95@gmail.com>
2019-07-15 13:14:39 +0100

    Project setup using onoff

---
+++ test2.js
@@ -0,0 +1,12 @@
+
+const Gpio = require('onoff').Gpio; // Gpio class
+const led = new Gpio(17, 'out');       // Export GPIO17 as an output
+console.log(led);
+led.writeSync(1);
+
+setTimeout(_ => {
+console.log("Stop");
+  led.unexport();    // Unexport GPIO and free resources
+}, 5000);
+
+
